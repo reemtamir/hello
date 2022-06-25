8ef7cb00f857d06f81dcf88d8ef3768742f5eb35
Reem Tamir <reem.tamir36@gmail.com>
2022-06-25 12:46:46 +0300

    add pop function

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 'use strict';
+var pop = new Audio('sounds/balloon.mp3');
 
 const changeColor = () => {
   const randomColor = Math.floor(Math.random() * 16777215).toString(16);
@@ -22,7 +23,7 @@ function getRandomStyles() {
   var b = random(255);
   var mt = random(200);
   var ml = random(50);
-  var dur = random(5) + 5;
+  var dur = random(20) + 19;
   return `
   background-color: rgba(${r},${g},${b},0.7);
   color: rgba(${r},${g},${b},0.7); 
@@ -31,14 +32,28 @@ function getRandomStyles() {
   animation: float ${dur}s ease-in infinite
   `;
 }
-
+var count = 0;
 function createBalloons(num) {
   var balloonContainer = document.getElementById('balloon-container');
-  for (var i = num; i > 0; i--) {
+  for (let i = num; i > 0; i--) {
     var balloon = document.createElement('div');
     balloon.className = 'balloon';
+    balloon.id = i;
     balloon.style.cssText = getRandomStyles();
     balloonContainer.append(balloon);
+    balloon.addEventListener('click', function balloonPop(event, id) {
+      var elBtn = document.querySelector('.btn');
+      var elH2 = document.querySelector('h2');
+      document.getElementById(i).classList.add('hide');
+      pop.play();
+      pop.currentTime = 0;
+      count++;
+      if (count === 3) {
+        elBtn.classList.remove('none');
+
+        elH2.innerHTML = 'כעת ניתן לשלוח את הטופס';
+      }
+    });
   }
 }
 
